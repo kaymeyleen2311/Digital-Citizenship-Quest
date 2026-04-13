ddab14edcbbd654a9fc03fa1efdf4572c97b31ab
kaymeyleen2311 <kathleengarciainjun11@gmail.com>
2026-04-13 14:39:06 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hero-hub-v4';
+const CACHE_NAME = 'hero-hub-v5';
 const urlsToCache = [
   './',
   './index.html',
@@ -8,13 +8,15 @@ const urlsToCache = [
   './game3.html', 
   './game4.html', 
   './game5.html',
-  './TheTeam.png'
+  './TheTeam.png',
+  // ISAMA ANG MGA ITO PARA SA DESIGN AT EFFECTS:
+  'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      
       return Promise.allSettled(
         urlsToCache.map(url => cache.add(url))
       );
